c8b68e0a7b3b857655f3ac9feaebb94a25a2560c
heo hyunjun <hhj8276@naver.com>
2023-04-26 13:02:01 +0900

    외부 프로젝트 클론

---
+++ XCAChatGPT/XCAChatGPTApp.swift
@@ -0,0 +1,30 @@
+//
+//  XCAChatGPTApp.swift
+//  XCAChatGPT
+//
+//  Created by Alfian Losari on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct XCAChatGPTApp: App {
+    
+    @StateObject var vm = ViewModel(api: ChatGPTAPI(apiKey: "PROVIDED_KEY"))
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView(vm: vm)
+                    .toolbar {
+                        ToolbarItem {
+                            Button("Clear") {
+                                vm.clearMessages()
+                            }
+                            .disabled(vm.isInteractingWithChatGPT)
+                        }
+                    }
+            }
+        }
+    }
+}
