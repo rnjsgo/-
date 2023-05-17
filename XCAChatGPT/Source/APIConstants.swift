d6169b69546d0d804dab23c8b4c10d98b925489e
rnjsgo <rnjsgo2008@naver.com>
2023-05-17 15:49:50 +0900

    feat: success getQuestion
    yes

---
+++ XCAChatGPT/Source/APIConstants.swift
@@ -0,0 +1,14 @@
+//
+//  APIConstants.swift
+//  XCAChatGPT
+//
+//  Created by sun on 2023/05/10.
+//
+
+import Foundation
+
+struct APIConstants{
+    static let baseURL="http://localhost:8080"
+    
+    static let getCreativityURL=baseURL+"/question/creativity"
+}
